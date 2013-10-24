6b17eea425950088412bcacf53af455f7dcac564
Justin Vallelonga <jlvallelonga@gmail.com>
2013-10-23 22:09:58 -0600

    added comments

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+//function that logs hello
 module.exports.sayHello = function() {
 	console.log('Hello World!!!');
 }
 
+//function that logs goodbye
 module.exports.sayGoodbye = function() {
 	console.log('Goodbye');
 }
